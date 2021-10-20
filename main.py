568bd516a6313e7a460751e06420858a07c333b8
conflick0 <jackhsu0099@gmail.com>
2021-10-20 15:04:26 +0800

    test basic rdd

--- main.py
+++ main.py
@@ -1,44 +1,10 @@
-from pyspark.sql import SparkSession
+from pyspark import SparkContext, SparkConf
+conf = SparkConf().setAppName('Test').setMaster('local')
+sc = SparkContext(conf=conf)
+sc.setLogLevel("ERROR")  # set log error
 
+data = [1, 2, 3, 4, 5]
+rdd = sc.parallelize(data)
 
-class Task1:
-    def __init__(self, col_name):
-        self.col_name = col_name
-        self.max = None
-        self.min = None
-        self.count = None
-
-
-def cal_task1(df, col_name):
-    task1 = Task1(col_name)
-    task1.max = df.agg({'Global_active_power': 'max'}).first()[0]
-    task1.min = df.agg({'Global_active_power': 'min'}).first()[0]
-    task1.count = df.agg({'Global_active_power': 'count'}).first()[0]
-    return task1
-
-
-class ColumnNames:
-    global_active_power = 'Global_active_power'
-    global_reactive_power = 'Global_reactive_power'
-    voltage = 'Voltage'
-    global_intensity = 'Global_intensity'
-
-
-if __name__ == '__main__':
-    spark = SparkSession.builder.appName("DataFrame").getOrCreate()
-    data_file = 'household_power_consumption.txt'
-
-    df = spark.read.load(
-        data_file,
-        format='csv',
-        sep=';',
-        inferSchema='true',
-        header='true',
-        nullValue='?'
-    )
-
-    gap_task1_result = cal_task1(df, ColumnNames.global_active_power)
-    print(f'{gap_task1_result.col_name}: '
-          f'max:{gap_task1_result.max}, '
-          f'min:{gap_task1_result.min}, '
-          f'count:{gap_task1_result.count}')
+print(f'### Results:\n {rdd.collect()}')
+sc.stop()
