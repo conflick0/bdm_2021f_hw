98dee82fa3d49039cb1f3396f98c467725f46514
conflick0 <jackhsu0099@gmail.com>
2021-10-21 22:43:02 +0800

    cal single col max, min and count by map reduce

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from pyspark import SparkContext, SparkConf
-from pyspark.sql import SparkSession
 
 class ColumnNames:
     global_active_power = 'Global_active_power'
@@ -8,28 +7,60 @@ class ColumnNames:
     global_intensity = 'Global_intensity'
 
 
+def isfloat(value):
+  try:
+    float(value)
+    return True
+  except ValueError:
+    return False
+
+
+def parseLine(line):
+    '''parsing the input data'''
+    fields = line.split(';')
+    datetime = ''.join([fields[0], fields[1]])
+
+    target_idxs = [2, 3]
+    target_fields = [datetime]
+    for i in target_idxs:
+        if isfloat(fields[i]):
+            target_fields.append(float(fields[i]))
+        else:
+            target_fields.append(None)
+
+    return target_fields[1]
+
+
 if __name__ == '__main__':
+    # set data file path
     data_file = 'household_power_consumption.txt'
     
+    # init spark
     conf = SparkConf().setAppName('Test').setMaster('local')
     sc = SparkContext(conf=conf)
-    spark = SparkSession(sc)
 
-    spark.sparkContext.setLogLevel('WARN') # turn off log info
-    sc.setLogLevel("WARN")  # turn off log info
+    # turn off log info
+    sc.setLogLevel("WARN")  
 
-    df = spark.read.load(
-        data_file,
-        format='csv',
-        sep=';',
-        inferSchema='true',
-        header='true',
-        nullValue='?'
-    )
+    # read data
+    data = sc.textFile(data_file)
 
-    rows = df.select(
-        ColumnNames.global_active_power,
-        ColumnNames.global_reactive_power,
-        ColumnNames.voltage,
-        ColumnNames.global_intensity
-    ).describe().show()
+    # remove header
+    header = data.first()
+    data = data.filter(lambda row: row != header)
+
+    # extract target fields
+    data = data.map(parseLine)
+
+    # remove null value
+    data = data.filter(lambda x: x is not None)
+
+    # cal max, min and count
+    max_val = data.reduce(lambda a, b: a if a > b else b)
+    min_val = data.reduce(lambda a, b: a if a < b else b)
+    count_val = data.map(lambda x: 1).reduce(lambda a, b: a + b)
+
+    # show result
+    print(max_val)
+    print(min_val)
+    print(count_val)
