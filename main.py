9c04695c92a70c7044e31a1a073e934b90191bb9
conflick0 <jackhsu0099@gmail.com>
2021-10-21 19:12:30 +0800

    cal min, max and count by df desc

--- main.py
+++ main.py
@@ -1,10 +1,35 @@
 from pyspark import SparkContext, SparkConf
-conf = SparkConf().setAppName('Test').setMaster('local')
-sc = SparkContext(conf=conf)
-sc.setLogLevel("ERROR")  # set log error
+from pyspark.sql import SparkSession
 
-data = [1, 2, 3, 4, 5]
-rdd = sc.parallelize(data)
+class ColumnNames:
+    global_active_power = 'Global_active_power'
+    global_reactive_power = 'Global_reactive_power'
+    voltage = 'Voltage'
+    global_intensity = 'Global_intensity'
 
-print(f'### Results:\n {rdd.collect()}')
-sc.stop()
+
+if __name__ == '__main__':
+    data_file = 'household_power_consumption.txt'
+    
+    conf = SparkConf().setAppName('Test').setMaster('local')
+    sc = SparkContext(conf=conf)
+    spark = SparkSession(sc)
+
+    spark.sparkContext.setLogLevel('WARN') # turn off log info
+    sc.setLogLevel("WARN")  # turn off log info
+
+    df = spark.read.load(
+        data_file,
+        format='csv',
+        sep=';',
+        inferSchema='true',
+        header='true',
+        nullValue='?'
+    )
+
+    rows = df.select(
+        ColumnNames.global_active_power,
+        ColumnNames.global_reactive_power,
+        ColumnNames.voltage,
+        ColumnNames.global_intensity
+    ).describe().show()
