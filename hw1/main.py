6517c9489eab6e789d2e5884bcd12733c507a305
conflick0 <jackhsu0099@gmail.com>
2021-10-23 12:11:01 +0800

    cal max by key value pair

--- hw1/main.py
+++ hw1/main.py
@@ -1,34 +1,29 @@
 from pyspark import SparkContext, SparkConf
 
-class ColumnNames:
+class Schema:
     global_active_power = 'Global_active_power'
     global_reactive_power = 'Global_reactive_power'
     voltage = 'Voltage'
     global_intensity = 'Global_intensity'
 
 
-def isfloat(value):
+def try_float(value):
   try:
-    float(value)
-    return True
+    value = float(value)
+    return value
   except ValueError:
-    return False
+    return None
 
 
-def parseLine(line):
+def parse_line(line):
     '''parsing the input data'''
     fields = line.split(';')
-    datetime = ''.join([fields[0], fields[1]])
-
-    target_idxs = [2, 3]
-    target_fields = [datetime]
-    for i in target_idxs:
-        if isfloat(fields[i]):
-            target_fields.append(float(fields[i]))
-        else:
-            target_fields.append(None)
-
-    return target_fields[1]
+    return (
+        (Schema.global_active_power, try_float(fields[2])),
+        (Schema.global_reactive_power, try_float(fields[3])),
+        (Schema.voltage, try_float(fields[4])),
+        (Schema.global_intensity, try_float(fields[5]))
+    )
 
 
 if __name__ == '__main__':
@@ -47,36 +42,39 @@ if __name__ == '__main__':
 
     # remove header
     header = data.first()
-    data = data.filter(lambda row: row != header)
+    data = data.filter(lambda x: x != header)
 
     # extract target fields
-    data = data.map(parseLine)
+    data = data.map(parse_line)
 
-    # remove null value
-    data = data.filter(lambda x: x is not None)
+    # build key value pair (field_name, value)
+    data = data.flatMap(lambda x: x)
 
-    # cal max, min and count
-    max_val = data.reduce(lambda a, b: a if a > b else b)
-    min_val = data.reduce(lambda a, b: a if a < b else b)
-    count_val = data.map(lambda x: 1).reduce(lambda a, b: a + b)
+    # # remove null value
+    data = data.filter(lambda x: x[1] is not None)
 
-    # cal mean by map reduce
-    sum_val, count_val = data.map(lambda x: (x, 1))\
-        .reduce(lambda a, b: (a[0] + b[0], a[1] + b[1]))
+    # # cal max, min and count
+    max_val = data.reduceByKey(lambda a, b: a if a > b else b)
+    # min_val = data.reduce(lambda a, b: a if a < b else b)
+    # count_val = data.map(lambda x: 1).reduce(lambda a, b: a + b)
+
+    # # cal mean by map reduce
+    # sum_val, count_val = data.map(lambda x: (x, 1))\
+    #     .reduce(lambda a, b: (a[0] + b[0], a[1] + b[1]))
     
-    mean_val = sum_val / count_val
+    # mean_val = sum_val / count_val
 
-    # cal mean by agg
-    sum_val2, count_val2 = data.aggregate(
-        (0, 0),
-        (lambda acc, value: (acc[0] + value, acc[1] + 1)),
-        (lambda acc1, acc2: (acc1[0] + acc2[0], acc1[1] + acc2[1]))
-    )
-    mean_val2 = sum_val2 / count_val2
+    # # cal mean by agg
+    # sum_val2, count_val2 = data.aggregate(
+    #     (0, 0),
+    #     (lambda acc, value: (acc[0] + value, acc[1] + 1)),
+    #     (lambda acc1, acc2: (acc1[0] + acc2[0], acc1[1] + acc2[1]))
+    # )
+    # mean_val2 = sum_val2 / count_val2
 
-    # show result
-    print(max_val)
-    print(min_val)
-    print(count_val)
-    print(mean_val)
-    print(mean_val2)
+    # # show result
+    print(max_val.collect())
+    # print(min_val)
+    # print(count_val)
+    # print(mean_val)
+    # print(mean_val2)
