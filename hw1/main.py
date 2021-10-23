2b8c678b9506a10047f20e40c21ec9a3f1f90cfa
conflick0 <jackhsu0099@gmail.com>
2021-10-23 16:41:15 +0800

    cal std by key value pair

--- hw1/main.py
+++ hw1/main.py
@@ -1,10 +1,5 @@
+import math
 from pyspark import SparkContext, SparkConf
-
-class Schema:
-    global_active_power = 'Global_active_power'
-    global_reactive_power = 'Global_reactive_power'
-    voltage = 'Voltage'
-    global_intensity = 'Global_intensity'
 
 
 def try_float(value):
@@ -19,17 +14,25 @@ def parse_line(line):
     '''parsing the input data'''
     fields = line.split(';')
     return (
-        (Schema.global_active_power, try_float(fields[2])),
-        (Schema.global_reactive_power, try_float(fields[3])),
-        (Schema.voltage, try_float(fields[4])),
-        (Schema.global_intensity, try_float(fields[5]))
+        (0, try_float(fields[2])),
+        (1, try_float(fields[3])),
+        (2, try_float(fields[4])),
+        (3, try_float(fields[5]))
     )
 
 
 if __name__ == '__main__':
+    # schema
+    schema = [
+        'Global_active_power',
+        'Global_reactive_power',
+        'Voltage',
+        'Global_intensity'
+    ]
+
     # set data file path
     data_file = 'household_power_consumption.txt'
-    
+
     # init spark
     conf = SparkConf().setAppName('Test').setMaster('local')
     sc = SparkContext(conf=conf)
@@ -53,18 +56,33 @@ if __name__ == '__main__':
     # remove null value
     data = data.filter(lambda x: x[1] is not None)
 
-    # cal max, min and count
-    max_val = data.reduceByKey(lambda a, b: a if a > b else b)
-    min_val = data.reduceByKey(lambda a, b: a if a < b else b)
-    count_val = data.map(lambda x: (x[0], 1)).reduceByKey(lambda a, b: a + b)
+    # cal max
+    max_val = data.reduceByKey(lambda a, b: a if a > b else b)\
+        .map(lambda x: x[1]).collect()
 
-    # cal mean by map reduce
+    # cal min
+    min_val = data.reduceByKey(lambda a, b: a if a < b else b)\
+        .map(lambda x: x[1]).collect()
+    
+    # cal count
+    count_val = data.map(lambda x: (x[0], 1))\
+        .reduceByKey(lambda a, b: a + b)\
+        .map(lambda x: x[1]).collect()
+
+    # cal mean
     mean_val = data.mapValues(lambda x: (x, 1))\
         .reduceByKey(lambda a, b: (a[0] + b[0], a[1] + b[1]))\
-        .mapValues(lambda x: x[0]/x[1])
+        .mapValues(lambda x: x[0]/x[1])\
+        .map(lambda x: x[1]).collect()
     
+    # cal std
+    std_val = data.map(lambda x: (x[0], (x[1] - mean_val[x[0]])**2))\
+        .reduceByKey(lambda a, b: a + b)\
+        .map(lambda x: math.sqrt(x[1]/count_val[x[0]])).collect()
+
     # show result
-    print(max_val.collect())
-    print(min_val.collect())
-    print(count_val.collect())
-    print(mean_val.collect())
+    print(f'max: {max_val}')
+    print(f'min: {min_val}')
+    print(f'count: {count_val}')
+    print(f'mean: {mean_val}')
+    print(f'std: {std_val}')
