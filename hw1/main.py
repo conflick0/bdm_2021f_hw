4ba81fe9bfcaab6e77bb01f9986a7d465ff7ef4e
conflick0 <jackhsu0099@gmail.com>
2021-10-23 12:48:38 +0800

    cal mean by key value

--- hw1/main.py
+++ hw1/main.py
@@ -53,28 +53,18 @@ if __name__ == '__main__':
     # remove null value
     data = data.filter(lambda x: x[1] is not None)
 
-    # # cal max, min and count
+    # cal max, min and count
     max_val = data.reduceByKey(lambda a, b: a if a > b else b)
     min_val = data.reduceByKey(lambda a, b: a if a < b else b)
     count_val = data.map(lambda x: (x[0], 1)).reduceByKey(lambda a, b: a + b)
 
-    # # cal mean by map reduce
-    # sum_val, count_val = data.map(lambda x: (x, 1))\
-    #     .reduce(lambda a, b: (a[0] + b[0], a[1] + b[1]))
+    # cal mean by map reduce
+    mean_val = data.mapValues(lambda x: (x, 1))\
+        .reduceByKey(lambda a, b: (a[0] + b[0], a[1] + b[1]))\
+        .mapValues(lambda x: x[0]/x[1])
     
-    # mean_val = sum_val / count_val
-
-    # # cal mean by agg
-    # sum_val2, count_val2 = data.aggregate(
-    #     (0, 0),
-    #     (lambda acc, value: (acc[0] + value, acc[1] + 1)),
-    #     (lambda acc1, acc2: (acc1[0] + acc2[0], acc1[1] + acc2[1]))
-    # )
-    # mean_val2 = sum_val2 / count_val2
-
-    # # show result
+    # show result
     print(max_val.collect())
     print(min_val.collect())
     print(count_val.collect())
-    # print(mean_val)
-    # print(mean_val2)
+    print(mean_val.collect())
