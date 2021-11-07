3addbed8390794dbfa3185cbaa44b4a6d56e7ce7
conflick0 <jackhsu0099@gmail.com>
2021-11-07 18:57:16 +0800

    use spark read csv

--- hw2/main.py
+++ hw2/main.py
@@ -4,32 +4,22 @@ from pyspark.sql import SparkSession
 from pyspark import SparkContext, SparkConf
 
 
-def parse_line(line):
-    '''parsing the input data'''
-    fields = line.split(',')
-    return (
-        fields[0],
-        fields[1],
-        fields[2],
-        fields[4],
-        fields[5],
-        fields[6],
-        fields[7],
-    )
-
-
-def is_nan(x):
-    return x != x
-
-
-def extract_words(sentence):
+def extract_words(s):
     # check nan
-    if is_nan(sentence): return ['']
-    
-    # remove hex string
-    sentence = re.sub(r'[^\x00-\x7f]',r'', sentence) 
-    
-    return sentence.strip('"').split(" ")
+    if s is None: return ['']
+    s = re.sub(r'[^\x00-\x7f]',r'', s)
+    return tuple(s.strip('"').split(" "))
+
+
+def count_word_by_total(data, field_idx):
+    return data\
+        .map(lambda x: extract_words(x[field_idx]))\
+        .flatMap(lambda x: x)\
+        .map(lambda w: (w, 1)) \
+        .reduceByKey(lambda a, b: a + b)\
+        .map(lambda x: (x[1], [x[0]]))\
+        .reduceByKey(lambda a, b: a + b)\
+        .sortBy(lambda x: x[0], False)
 
 
 if __name__ == '__main__':
@@ -42,36 +32,19 @@ if __name__ == '__main__':
     sc.setLogLevel("ERROR")  
 
     # read data
-    data_file_path = 'hw2/data/News_Final.csv'
-    data = sc.textFile(data_file_path)
+    news_file = 'hw2/data/News_Final.csv'
+    news_data = sc.textFile(news_file)
+    news_data = spark.read.csv(news_data).rdd.map(tuple)
 
     # remove header
-    header = data.first()
-    data = data.filter(lambda x: x != header)
-
-    # extract target fields
-    raw_data = data.map(parse_line)
+    header = news_data.first()
+    news_data = news_data.filter(lambda x: x != header)
 
     # title total word count
-    title_wc = raw_data\
-        .map(lambda x: extract_words(x[1]))\
-        .flatMap(lambda x: x)\
-        .map(lambda w: (w, 1)) \
-        .reduceByKey(lambda a, b: a + b)\
-        .map(lambda x: (x[1], [x[0]]))\
-        .reduceByKey(lambda a, b: a + b)\
-        .sortBy(lambda x: x[0], False)
+    title_wc = count_word_by_total(news_data, field_idx=1)
 
     # head line total word count
-    head_line_wc = raw_data\
-        .map(lambda x: extract_words(x[2]))\
-        .flatMap(lambda x: x)\
-        .map(lambda w: (w, 1)) \
-        .reduceByKey(lambda a, b: a + b)\
-        .map(lambda x: (x[1], [x[0]]))\
-        .reduceByKey(lambda a, b: a + b)\
-        .sortBy(lambda x: x[0], False)
-    
+    head_line_wc = count_word_by_total(news_data, field_idx=2)
 
     print(title_wc.take(5))
     print(head_line_wc.take(5))
