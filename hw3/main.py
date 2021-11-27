afd9c62d363f33607daac71a2db5656e963c6093
conflick0 <jackhsu0099@gmail.com>
2021-11-27 18:25:04 +0800

    read files to docs

---
+++ hw3/main.py
@@ -0,0 +1,51 @@
+import re
+import string
+import time
+from functools import wraps
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import monotonically_increasing_id
+from pyspark import SparkContext, SparkConf
+
+
+def timer(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        exec_time = time.time()
+        func(*args, **kwargs)
+        exec_time = time.time() - exec_time
+        print(f'execution time: {exec_time} sec')
+    return wrapper
+
+
+def read_file(file_path):
+    '''return file content text'''
+    return spark.read.text(file_path, wholetext=True).rdd.collect()[0][0]
+
+
+def read_docs(file_paths):
+    docs = []
+    for fp in file_paths:
+        s = read_file(fp)
+        s =  s.replace('\n','').replace('\r', '')
+        bodys = re.findall(r'<BODY>(.+?)</BODY>', s)
+        docs.extend(bodys)
+    return docs
+
+
+if __name__ == '__main__':
+    # init spark
+    conf = SparkConf()\
+        .setAppName('hw3')\
+        .setMaster('spark://spark-1:7077')
+    sc = SparkContext(conf=conf)
+    
+    spark = SparkSession(sc)
+
+    # set log only error
+    sc.setLogLevel("ERROR")
+
+    file_paths = [f'hw3/data/reut2-{i:03}.sgm' for i in range(0, 22)]
+
+    docs = read_docs(file_paths)
+
+    print(len(docs))
