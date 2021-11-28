7d7ea5715b9edbde5a38eb26e5f3d25aec583dff
conflick0 <jackhsu0099@gmail.com>
2021-11-28 13:07:13 +0800

    build shingles

--- hw3/main.py
+++ hw3/main.py
@@ -8,6 +8,7 @@ from pyspark import SparkContext, SparkConf
 
 
 def timer(func):
+    '''cal execution time of func'''
     @wraps(func)
     def wrapper(*args, **kwargs):
         exec_time = time.time()
@@ -23,6 +24,7 @@ def read_file(file_path):
 
 
 def read_docs(file_paths):
+    '''return all docs'''
     docs = []
     for fp in file_paths:
         s = read_file(fp)
@@ -30,6 +32,19 @@ def read_docs(file_paths):
         bodys = re.findall(r'<BODY>(.+?)</BODY>', s)
         docs.extend(bodys)
     return docs
+
+
+def extract_words(s):
+    '''return word list from doc'''
+    if s is None: return ['']
+    s = re.sub(r'[^\x00-\x7f]',r'', s)
+    s = s.translate(str.maketrans('', '', string.punctuation))
+    return s.lower().split()
+
+
+def build_shingles(s, k):
+    '''return shingle set from doc'''
+    return list(set([tuple(s[i:(i+k)]) for i in range(len(s) - k + 1)]))
 
 
 if __name__ == '__main__':
@@ -47,5 +62,11 @@ if __name__ == '__main__':
     file_paths = [f'hw3/data/reut2-{i:03}.sgm' for i in range(0, 22)]
 
     docs = read_docs(file_paths)
+    
+    docs = sc.parallelize(docs)
 
-    print(len(docs))
+    doc_shingles = docs\
+        .map(lambda x: extract_words(x))\
+        .map(lambda x: build_shingles(x, k=8))
+
+    print(doc_shingles.take(1))
