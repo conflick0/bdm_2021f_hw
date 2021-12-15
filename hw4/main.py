3e45b04e47eacba8cb5f73ac35c16c9a84097d03
changken <pp24513gmail.com>
2021-12-15 21:15:36 +0800

    task4完成將資料填入矩陣中

--- hw4/main.py
+++ hw4/main.py
@@ -139,33 +139,52 @@ def task3(movies, ratings):
 
 @timer
 def task4(ratings):
-    # matrix = []
-    # for i in range(6040): # user
-    #     matrix.append([])
-    #     for j in range(3952): #movie
-    #         matrix[i].append(0)
 
-    # def buildMatrix(x):
-    #     global matrix
-
-    #     user = int(x[0]) - 1
-    #     movie = int(x[1]) - 1
-    #     rate = int(x[2])
-        
-    #     matrix[user][movie] = rate
-
-    # ratings.map(buildMatrix)
-
-    # for i in range(len(matrix[0])):
-    #     if matrix[0][i] > 0:
-    #         print('yes')
-
+    # 計算平均
     rdd = ratings.map(lambda x : (x[0], (int(x[2]), 1)))\
                 .reduceByKey(lambda a, b : (a[0] + b[0], a[1] + b[1]))\
                 .map(lambda x : (x[0], x[1][0] / x[1][1]))\
                 .sortBy(lambda x : int(x[0]))
+
+    #[('1', 3.4), ('2', 5), ('3', 4.5)]
+    averageMatrix = rdd.collect()
+
+    # 將ratings再次清理
+    def clearRatings(x):
+        user = int(x[0]) - 1
+        movie = int(x[1]) - 1
+        rate = int(x[2])
+        
+        return (user, (movie, rate))
+
+    ratingRDD = ratings.map(clearRatings)\
+                        .sortByKey()
+
+    # [(0, (1192, 5)), (0, (660, 3)), (0, (913, 3)), (0, (3407, 4)), (0, (2354, 5))]
+    # print(ratingRDD.take(5))
+
+    ratingBC = sc.broadcast(ratingRDD.collect())
+
+    matrix = []
+    for i in range(6040): # user
+        matrix.append([])
+        for j in range(3952): #movie
+            matrix[i].append(averageMatrix[i][1])
+
+    matrixRDD = sc.parallelize(matrix)\
+                .zipWithIndex()
+
+    def paddingValue(x):
+        rowData, i = x
+        for item in ratingBC.value:
+            if item[0] == i:
+                rowData[item[1][0]] = item[1][1]
+        
+        return rowData
+
+    matrixRDD = matrixRDD.map(paddingValue)
     
-    print(rdd.take(5))
+    print(matrixRDD.take(1))
 
 @timer
 def task5(ratings):
@@ -206,7 +225,7 @@ if __name__ == '__main__':
     # task3(movies, ratings)
 
     # task4
-    # task4(ratings)
+    task4(ratings)
 
     # task5
-    task5(ratings)
+    # task5(ratings)
